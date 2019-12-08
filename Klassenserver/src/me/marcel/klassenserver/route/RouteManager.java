70f6f70bf41d2a5168605a4e2c19def44dc2846e
1fabi0 <58549442+1fabi0@users.noreply.github.com>
2019-12-08 01:09:02 +0100

    updated a few things

--- Klassenserver/src/me/marcel/klassenserver/route/RouteManager.java
+++ Klassenserver/src/me/marcel/klassenserver/route/RouteManager.java
@@ -1,9 +1,7 @@
 package me.marcel.klassenserver.route;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import org.bukkit.Location;
 
 import me.marcel.klassenserver.config.ConfigManager;
@@ -11,13 +9,18 @@ import me.marcel.klassenserver.config.ConfigManager;
 public class RouteManager {
 
 	private static List<Route> routes = new ArrayList<Route>();
-	private static Map<String, Route> routesWithName = new HashMap<String, Route>();
 	
 	public static void add(Route route) {
 		if (!(exists(route.getName()))) {
 			routes.add(route);
-			routesWithName.put(route.getName(), route);
 		}
+	}
+	public static void update(Route route) {
+		for(int i = 0; i < routes.toArray().length ; i++) {
+			if(routes.get(i).getName().equalsIgnoreCase(route.getName())) {
+				routes.set(i, route);
+			}
+		}		
 	}
 	
 	public static boolean exists(String name) {
@@ -33,15 +36,21 @@ public class RouteManager {
 	}
 	
 	public static Route getRouteByName(String name) {
-		return routesWithName.get(name);
+		Route returnRoute = null;
+		for(Route route : routes) {
+			if(route.getName().equalsIgnoreCase(name)) {
+				returnRoute = route;
+			}
+		}
+		return returnRoute;
 	}
 	
 	public static void loadRoutes() {
 		for (String route : ConfigManager.editor("routes").getStringList("routes")) {
 			if (!(ConfigManager.editor("routes").getString(route + ".start.world") == null)) {
 				
-				List<Location> checkpoints = ConfigManager.editor("routes").getCheckpoints(route+".checkpoints"); 
-				Location start = ConfigManager.editor("routes").getLocation(route + ".start");
+				List<Location> checkpoints = ConfigManager.editor("routes").getCheckpoints(route + ".checkpoints"); 
+				Location start = ConfigManager.editor("routes").getLocation(route +".start");			
 				
 				new Route(route, start, checkpoints);
 				
