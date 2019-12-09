40aeda450ef6c4b064759fc54fbaf71ecefe5c8a
1fabi0 <fabiankamp3@gmail.com>
2019-12-09 22:42:30 +0100

    more changes

--- Klassenserver/src/me/marcel/klassenserver/event/EPlayerInteract.java
+++ Klassenserver/src/me/marcel/klassenserver/event/EPlayerInteract.java
@@ -24,75 +24,76 @@ public class EPlayerInteract implements Listener {
 	@EventHandler
 	public void onInteract(PlayerInteractEvent event) {
 		Player player = event.getPlayer();
-		if (event.getAction() == Action.RIGHT_CLICK_BLOCK || event.getAction() == Action.RIGHT_CLICK_AIR) {
-			if (event.getAction() == Action.RIGHT_CLICK_BLOCK){
-				if (event.getClickedBlock().getType() == Material.ACACIA_SIGN || event.getClickedBlock().getType() == Material.ACACIA_WALL_SIGN || event.getClickedBlock().getType() == Material.BIRCH_SIGN || event.getClickedBlock().getType() == Material.BIRCH_WALL_SIGN || event.getClickedBlock().getType() == Material.DARK_OAK_SIGN || event.getClickedBlock().getType() == Material.DARK_OAK_WALL_SIGN || event.getClickedBlock().getType() == Material.JUNGLE_SIGN || event.getClickedBlock().getType() == Material.JUNGLE_WALL_SIGN || event.getClickedBlock().getType() == Material.OAK_SIGN || event.getClickedBlock().getType() == Material.OAK_WALL_SIGN || event.getClickedBlock().getType() == Material.OAK_WALL_SIGN || event.getClickedBlock().getType() == Material.SPRUCE_SIGN || event.getClickedBlock().getType() == Material.SPRUCE_WALL_SIGN ) {
+		
+		if (event.getAction() == Action.RIGHT_CLICK_BLOCK){
+			if (event.getClickedBlock().getType() == Material.ACACIA_SIGN || event.getClickedBlock().getType() == Material.ACACIA_WALL_SIGN || event.getClickedBlock().getType() == Material.BIRCH_SIGN || event.getClickedBlock().getType() == Material.BIRCH_WALL_SIGN || event.getClickedBlock().getType() == Material.DARK_OAK_SIGN || event.getClickedBlock().getType() == Material.DARK_OAK_WALL_SIGN || event.getClickedBlock().getType() == Material.JUNGLE_SIGN || event.getClickedBlock().getType() == Material.JUNGLE_WALL_SIGN || event.getClickedBlock().getType() == Material.OAK_SIGN || event.getClickedBlock().getType() == Material.OAK_WALL_SIGN || event.getClickedBlock().getType() == Material.OAK_WALL_SIGN || event.getClickedBlock().getType() == Material.SPRUCE_SIGN || event.getClickedBlock().getType() == Material.SPRUCE_WALL_SIGN ) {
 				
-					Sign sign = (Sign) event.getClickedBlock().getState();
-				
-					List<String> lore = new ArrayList<String>();
-					boolean loreExist = true;
+				Sign sign = (Sign) event.getClickedBlock().getState();
+			
+				List<String> lore = new ArrayList<String>();
+				boolean loreExist = true;
+				try {
+					lore = event.getItem().getItemMeta().getLore();
+				}catch(Exception ex){
+					loreExist = false;
+				} 
+				if(loreExist){
 					try {
-						lore = event.getItem().getItemMeta().getLore();
-					}catch(Exception ex){
-						loreExist = false;
-					} 
-					if(loreExist){
-						try {
 						
 					
-							if(lore.get(0).equalsIgnoreCase("§b[§5Klassenserver§b]") && lore.get(1).equalsIgnoreCase("Klicke hiermit auf ein Schild") && lore.get(2).equalsIgnoreCase("und es wird zum Start Schild für das Jump'n Run:")) {				
+						if(lore.get(0).equalsIgnoreCase("§b[§5Klassenserver§b]") && lore.get(1).equalsIgnoreCase("Klicke hiermit auf ein Schild") && lore.get(2).equalsIgnoreCase("und es wird zum Start Schild für das Jump'n Run:")) {				
 							
-								String name = lore.get(3);
-								sign.setLine(0, "§b[§5Klassenserver§b]");
-								sign.setLine(1, "Jump'n Run:");
-								sign.setLine(2, name);
-								sign.setLine(3, "Klicke zum spielen");
+							String name = lore.get(3);
+							sign.setLine(0, "§b[§5Klassenserver§b]");
+							sign.setLine(1, "Jump'n Run:");
+							sign.setLine(2, name);
+							sign.setLine(3, "Klicke zum spielen");
 							
-								sign.update();						
-							}
-						} catch (Exception e) {
-					}	
+							sign.update();						
+						}
+					} catch (Exception e) {
+				}	
 					
-				}
-					if (sign.getLine(0).equalsIgnoreCase("§b[§5Klassenserver§b]")) {
-					String routeName = sign.getLine(2);
-					if (RouteManager.exists(routeName)) {
-						Route route = RouteManager.getRouteByName(routeName);
+			}
+			if (sign.getLine(0).equalsIgnoreCase("§b[§5Klassenserver§b]")) {
+				String routeName = sign.getLine(2);
+				if (RouteManager.exists(routeName)) {
+					Route route = RouteManager.getRouteByName(routeName);
 						
-						player.sendMessage("§8[§aKlassenserver§8] §c Du hast das Jump'n Run " + route.getName() + " betreten!");
-						Inventory inv = player.getInventory();
-						PlayerPlayingManager.add(player.getUniqueId(), route, player.getLocation(), inv);
-						inv.clear();
-						ItemStack stop = new ItemStack(Material.REDSTONE, 1);
-						ItemMeta stopMeta = stop.getItemMeta();
-						stopMeta.setDisplayName("§l§4Stop");
-						stopMeta.setLocalizedName("Stop");
-						List<String> stopLore = new ArrayList<String>();
-						stopLore.add("Rechts-klicke dieses Item");
-						stopLore.add("um das Jump'n Run zu stoppen");
-						stopMeta.setLore(stopLore);
-						stop.setItemMeta(stopMeta);
-						inv.setItem(8, stop);
-						ItemStack backToCheckpoint = new ItemStack(Material.BLAZE_ROD, 1);
-						ItemMeta checkpointMeta = backToCheckpoint.getItemMeta();
-						checkpointMeta.setDisplayName("§l§6Zurück zum letzen Checkpoint");
-						checkpointMeta.setLocalizedName("Checkpoint");
-						List<String> checkpointLore = new ArrayList<String>();
-						checkpointLore.add("Rechts-klicke dieses Item");
-						checkpointLore.add("um zum letzen Checkpoint zu gelangen");
-						checkpointMeta.setLore(checkpointLore);
-						backToCheckpoint.setItemMeta(checkpointMeta);
-						inv.setItem(0, backToCheckpoint);
-						player.getInventory().setContents(inv.getContents());
-						player.updateInventory();
-						player.teleport(route.getStart());
-					}
+					player.sendMessage("§8[§aKlassenserver§8] §c Du hast das Jump'n Run " + route.getName() + " betreten!");
+					Inventory inv = player.getInventory();
+					PlayerPlayingManager.add(player.getUniqueId(), route, player.getLocation(), inv);
+					inv.clear();
+					ItemStack stop = new ItemStack(Material.REDSTONE, 1);
+					ItemMeta stopMeta = stop.getItemMeta();
+					stopMeta.setDisplayName("§l§4Stop");
+					stopMeta.setLocalizedName("Stop");
+					List<String> stopLore = new ArrayList<String>();
+					stopLore.add("Rechts-klicke dieses Item");
+					stopLore.add("um das Jump'n Run zu stoppen");
+					stopMeta.setLore(stopLore);
+					stop.setItemMeta(stopMeta);
+					inv.setItem(8, stop);
+					ItemStack backToCheckpoint = new ItemStack(Material.BLAZE_ROD, 1);
+					ItemMeta checkpointMeta = backToCheckpoint.getItemMeta();
+					checkpointMeta.setDisplayName("§l§6Zurück zum letzen Checkpoint");
+					checkpointMeta.setLocalizedName("Checkpoint");
+					List<String> checkpointLore = new ArrayList<String>();
+					checkpointLore.add("Rechts-klicke dieses Item");
+					checkpointLore.add("um zum letzen Checkpoint zu gelangen");
+					checkpointMeta.setLore(checkpointLore);
+					backToCheckpoint.setItemMeta(checkpointMeta);
+					inv.setItem(0, backToCheckpoint);
+					player.getInventory().setContents(inv.getContents());
+					player.updateInventory();
+					player.teleport(route.getStart());
 				}
 			}
 		}
+	}
+	if (event.getAction() == Action.RIGHT_CLICK_BLOCK || event.getAction() == Action.RIGHT_CLICK_AIR) {
 		boolean eventItem=true;
-		Material item = Material.AIR;
+			Material item = Material.AIR;
 		try{
 			item = event.getItem().getType();
 		}catch(Exception ex){
